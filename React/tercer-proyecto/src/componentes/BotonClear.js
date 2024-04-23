2aaa305f7aca3c5de2c7c9c36142f55ff35b80f5
paupallares <ppaupallares@gmail.com>
2024-04-23 14:42:05 +0200

    React - proyect 3 - calculator

---
+++ React/tercer-proyecto/src/componentes/BotonClear.js
@@ -0,0 +1,8 @@
+import '../styles/BotonClear.css'
+
+const BotonClear = (props) => (
+    <div className='boton-clear' onClick={props.manejarClear}>
+        {props.children}
+    </div>
+)
+export default BotonClear;
---
+++ React/tercer-proyecto/src/componentes/Pantalla.js
@@ -0,0 +1,9 @@
+import '../styles/Pantalla.css'
+
+const Pantalla = ({ input }) => (
+  <div className='input'>
+    {input}
+  </div>
+)
+
+export default Pantalla;
